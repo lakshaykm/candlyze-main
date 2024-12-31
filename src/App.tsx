2c254255221fa81c98100770895f1c6ea212fb96
lakshaykm <work.eartheuphoria@gmail.com>
2024-12-31 17:46:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { MainApp } from './pages/MainApp';
 import { About } from './pages/About';
 import { Terms } from './pages/Terms';
-import { subscription } from './pages/subscription';
+import { Subscription } from './pages/Subscription';
 import { Privacy } from './pages/Privacy';
 import { Refund } from './pages/Refund';
 import { Contact } from './pages/Contact';
@@ -14,7 +14,7 @@ export default function App() {
       <Route path="/" element={<MainApp />} />
       <Route path="/about" element={<About />} />
       <Route path="/terms" element={<Terms />} />
-      <Route path="/subscription" element={<subscription />} />
+      <Route path="/subscription" element={<Subscription />} />
       <Route path="/privacy" element={<Privacy />} />
       <Route path="/refund" element={<Refund />} />
       <Route path="/contact" element={<Contact />} />
