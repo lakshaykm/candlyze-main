117463f42e855a17c6e6f341d162e2b2b675ce59
lakshaykm <work.eartheuphoria@gmail.com>
2025-01-14 20:18:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,11 @@ import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { ForgotPassword } from './pages/ForgotPassword';
 import { ResetPassword } from './pages/ResetPassword';
+import { KeyLevelsAnalysis } from './pages/KeyLevelsAnalysis';
+import { TrendAnalysis } from './pages/TrendAnalysis';
+import { PatternRecognition } from './pages/PatternRecognition';
+import { IndicatorAnalysis } from './pages/IndicatorAnalysis';
+import { PricePrediction } from './pages/PricePrediction';
 
 export default function App() {
   return (
@@ -21,7 +26,11 @@ export default function App() {
       <Route path="/features" element={<Features />} />
       <Route path="/about" element={<About />} />
       <Route path="/app" element={<MainApp />} />
-      <Route path="/app/analysis/*" element={<MainApp />} />
+      <Route path="/app/analysis/key-levels" element={<KeyLevelsAnalysis />} />
+      <Route path="/app/analysis/trend" element={<TrendAnalysis />} />
+      <Route path="/app/analysis/patterns" element={<PatternRecognition />} />
+      <Route path="/app/analysis/indicators" element={<IndicatorAnalysis />} />
+      <Route path="/app/analysis/prediction" element={<PricePrediction />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/subscription" element={<Subscription />} />
       <Route path="/privacy" element={<Privacy />} />
