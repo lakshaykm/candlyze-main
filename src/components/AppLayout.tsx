b7c4cb5ff71e8f943a87d4d80c5fbf746447841e
lakshaykm <work.eartheuphoria@gmail.com>
2025-01-19 00:16:24 +0530

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -11,7 +11,7 @@ export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col ml-64">
         <AppHeader />
         <main className="flex-1 p-8">
           {children}
