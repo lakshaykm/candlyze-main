9050771b6fa6ee8356c31a259944761306ba2084
lakshaykm <work.eartheuphoria@gmail.com>
2025-02-26 14:07:15 +0530

    Delete razorpay-integration directory

--- razorpay-integration/routes/subscription.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createSubscription } = require('../controllers/subscriptionController');
-const router = express.Router();
-
-router.post('/create', createSubscription);
-
-module.exports = router;
--- razorpay-integration/routes/webhook.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { handleWebhook } = require('../controllers/webhookController');
-const router = express.Router();
-
-router.post('/', handleWebhook);
-
-module.exports = router;
